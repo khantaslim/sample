8ad3a69b31e7eef5aa83b1608e061bd59d1afc02
khantaslim <taslimlinkites@gmail.com>
2016-09-17 18:49:07 +0530

    change file in auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,19 +1,19 @@
 module.exports = {
 
     'facebookAuth': {
-        'clientID': '1652572908399327', // your App ID
-        'clientSecret': 'dcf1479233c8fa6c067f53075daeb25b', // your App Secret
+        'clientID': '', // your App ID
+        'clientSecret': '', // your App Secret
         'callbackURL': 'http://localhost:8000/auth/facebook/callback'
     },
     'twitterAuth': {
-        'consumerKey': 'pyaYloQ33k6fsYAiouKVAnXJp',
-        'consumerSecret': 'IOQZwAu8h9p2XKiTzwM9IARhElc1EVkjmV7UasUoqUu6L8QpMN',
+        'consumerKey': '',
+        'consumerSecret': '',
         'callbackURL': 'http://localhost:8000/auth/twitter/callback'
     },
     'googleAuth': {
-        'clientID': '511453329325-m23dfl06bo6vn763ir2p5s1u8et63lm5.apps.googleusercontent.com',
+        'clientID': '',
 
-        'clientSecret': 'a-l3mLiIV953MJJolBRUPaQ_',
+        'clientSecret': '',
         'callbackURL': 'http://localhost:8000/auth/google/callback'
     }
 
